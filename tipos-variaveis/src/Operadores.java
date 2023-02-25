c5e4735b1239f80e033f8ede94f5a9a81eb45c9f
Amanda Seabra <amanda-seabra@hotmail.com>
2023-02-25 20:59:12 -0300

    terceira alteração

--- tipos-variaveis/src/Operadores.java
+++ tipos-variaveis/src/Operadores.java
@@ -1,3 +1,28 @@
 public class Operadores {
     
+    public static void main(String[] args) {
+        boolean condicao1=true;
+
+        boolean condicao2=true;
+
+        if(condicao1 && condicao2){
+            System.out.println("as duas condições são verdadeiras");
+
+        }
+
+        if(condicao1 || condicao2){
+            System.out.println("Uma das condições é verdadeiras");
+
+        }
+
+
+            System.out.println("fim");
+
+
+
+
+        
+
+        
+    }
 }
